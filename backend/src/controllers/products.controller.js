6a24e768241585d8ab0c04680550f180fcb6345f
Ayush Gade <ayushgade23@gmail.com>
2025-12-27 02:23:03 +0530

    Final Changes

--- backend/src/controllers/products.controller.js
+++ backend/src/controllers/products.controller.js
@@ -1,4 +1,5 @@
 import prisma from "../services/prisma.js";
+import { createProductSchema } from "../validators/product.schema.js";
 
 export const getProducts = async (req, res, next) => {
   try {
@@ -51,5 +52,27 @@ export const deleteProduct = async (req, res, next) => {
     res.json({ message: "Product deleted successfully" });
   } catch (err) {
     next(err);
+  }
+};
+
+export const updateProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    // Validate body
+    const data = createProductSchema.parse(req.body);
+
+    const updated = await prisma.product.update({
+      where: { id },
+      data: {
+        productName: data.productName,
+        casNumber: data.casNumber,
+        unit: data.unit,
+      },
+    });
+
+    res.json(updated);
+  } catch (err) {
+    next(err); // VERY IMPORTANT
   }
 };
